40fac947485efbc370767f26443ffe6b05675de0
Erick Navarro <erick@navarro.io>
2015-12-18 22:52:42 -0500

    add translations

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,10 +1,11 @@
 defmodule IssueReporter.PageController do
   use IssueReporter.Web, :controller
+  import IssueReporter.Gettext
 
   def index(conn, _params) do
     issue_types = [
-      %{key: "broken semaphore", value: "Broken Semaphore"},
-      %{key: "broken lamp post", value: "Broken Lamp post"},
+      %{key: "broken semaphore", value: gettext("Broken semaphore")},
+      %{key: "broken lamp post", value: gettext("Broken lamp post")},
     ]
     render conn, "index.html",  issue_types: issue_types
   end
